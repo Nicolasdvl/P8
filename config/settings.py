f424aea66f3d35de1e2dd053df917abc7a6afc0d
Daval Nicolas <nicolasdvl@vivaldi.net>
2021-10-29 17:35:41 +0200

    fix deploiment error

--- config/settings.py
+++ config/settings.py
@@ -50,10 +50,8 @@ INSTALLED_APPS = [
     "off.apps.OffConfig",
     "search.apps.SearchConfig",
     "authentification.apps.AuthentificationConfig",
-    "crispy_forms",
 ]
 
-CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
