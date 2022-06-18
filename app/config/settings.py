533abb885dc1659be4fb632bd7acfbbd4728d282
Daval Nicolas <nicolasdvl@vivaldi.net>
2022-06-18 17:39:16 +0200

    restore dotenv

--- app/config/settings.py
+++ app/config/settings.py
@@ -16,6 +16,9 @@ import config.tasks
 import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
