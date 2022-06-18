e846f762a6a43e632b97ad37d232619b0e163974
Daval Nicolas <nicolasdvl@vivaldi.net>
2022-06-18 15:48:54 +0200

    setup sentry

--- app/config/settings.py
+++ app/config/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from celery.schedules import crontab
 import config.tasks
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -163,3 +165,19 @@ CELERY_BEAT_SCHEDULE = {
         "schedule": crontab(minute=0, hour=3, day_of_week=1),
     },
 }
+
+# Sentry integration
+
+sentry_sdk.init(
+    dsn="https://c8af00951ed64d7ea5ef1baa94b8be34@o1292167.ingest.sentry.io/6513634",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
